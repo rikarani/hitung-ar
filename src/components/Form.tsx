2c99236437479e23a8533ea9f19c1c5428238852
rikarani <46773591+rikarani@users.noreply.github.com>
2023-09-25 22:29:39 +0700

    rimek ui, bikin validasi

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,28 +1,50 @@
-import { Input, Typography, Button, Card } from "@material-tailwind/react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Input, Typography, Button } from "@material-tailwind/react";
 
 export default function Form(): React.JSX.Element {
+  const [target, setTarget] = useState<number>(0);
+
+  const schema = z.object({
+    sekarang: z.coerce.number().min(1, { message: "Minimal AR 1" }).max(60, { message: "Maksimal AR 60" }),
+    target: z.coerce
+      .number()
+      .min(target + 1, { message: "Target Salah" })
+      .max(60, { message: "Salah" }),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+  });
+
+  const { name, onBlur, ref } = register("sekarang");
+
   return (
-    <Card color="transparent" className="mb-4">
-      <Typography variant="lead" color="gray" className="text-white">
-        Input
-      </Typography>
-      <form className="mt-2 space-y-3.5">
-        <div>
-          <Input type="number" variant="outlined" label="AR Sekarang" color="white" />
-          <Typography color="red" className="font-normal text-red-200">
-            pala bapak engkau
-          </Typography>
+    <div>
+      <form className="mt-1.5" onSubmit={handleSubmit((data) => console.log({ ...data }))}>
+        <Typography variant="lead" color="white">
+          Input
+        </Typography>
+        <div className="mt-2.5 space-y-4">
+          <div>
+            <Input name={name} onBlur={onBlur} ref={ref} onChange={(e) => setTarget(parseInt(e.target.value))} label="AR Sekarang" variant="outlined" color="white" />
+            <Typography className="mt-0.5 text-red-200">{errors?.sekarang?.message || ""}</Typography>
+          </div>
+          <div>
+            <Input {...register("target")} label="Target AR" variant="outlined" color="white" />
+            <Typography className="text-red-200">{errors?.target?.message || ""}</Typography>
+          </div>
+          <Button type="submit" fullWidth variant="outlined" className="border-slate-300 py-1.5 text-lg capitalize text-slate-300 focus:ring-0">
+            Hitung
+          </Button>
         </div>
-        <div>
-          <Input type="number" variant="outlined" label="AR Sekarang" color="white" />
-          <Typography color="red" className="font-normal text-red-200">
-            pala bapak engkau
-          </Typography>
-        </div>
-        <Button onClick={() => alert("pala bapak engkau")} fullWidth variant="outlined" className="border-slate-300 py-1.5 text-lg capitalize text-slate-300 focus:ring-0">
-          Hitung
-        </Button>
       </form>
-    </Card>
+    </div>
   );
 }
