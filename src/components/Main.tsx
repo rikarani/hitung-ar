47b0c1aea9456150a5a8916a09dce9280a5d343c
rikarani <46773591+rikarani@users.noreply.github.com>
2023-09-26 05:30:25 +0700

    rimek ui

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,7 +4,7 @@ import Result from "./Result";
 
 export default function Main(): React.JSX.Element {
   return (
-    <Card color="transparent" className="mt-3 divide-y divide-white rounded-md bg-slate-700 px-3 py-2">
+    <Card color="transparent" className="mt-3 divide-y divide-white rounded-none bg-slate-700 px-3 py-2 sm:rounded-2xl">
       <Result />
       <Form />
     </Card>
