c7383a34c44d4627f37414da9ecabca20bdfe2bd
rikarani <46773591+rikarani@users.noreply.github.com>
2023-09-25 22:29:52 +0700

    ui sementara

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -3,14 +3,14 @@ import { Typography, Progress } from "@material-tailwind/react";
 export default function Result(): React.JSX.Element {
   return (
     <div>
-      <Typography variant="lead" color="gray" className="mt-2 text-white">
+      <Typography variant="lead" color="gray" className="text-white">
         Result
       </Typography>
       <div>
-        <Typography variant="paragraph" color="gray" className="mb-0.5 text-white">
+        <Typography variant="paragraph" color="gray" className="text-white">
           Butuh 100 Hari Lagi Untuk Mencapai AR 60
         </Typography>
-        <Progress value={100} size="sm" label="Completed" />
+        <Progress value={100} size="lg" label="Completed" className="mb-3" />
       </div>
     </div>
   );
