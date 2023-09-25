7ae46269da3caf810814243c0ef7dbd38777718a
rikarani <46773591+rikarani@users.noreply.github.com>
2023-09-25 22:29:23 +0700

    rimek ui

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,12 @@
+import { Card } from "@material-tailwind/react";
 import Form from "./Form";
 import Result from "./Result";
 
 export default function Main(): React.JSX.Element {
   return (
-    <div className="mt-3 divide-y divide-white rounded-md bg-slate-700 px-3 py-2">
-      <Form />
+    <Card color="transparent" className="mt-3 divide-y divide-white rounded-md bg-slate-700 px-3 py-2">
       <Result />
-    </div>
+      <Form />
+    </Card>
   );
 }
