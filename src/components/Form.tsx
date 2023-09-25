e642671f9fc4d9e644129eddfadaffa4c1c4fc55
rikarani <46773591+rikarani@users.noreply.github.com>
2023-09-25 14:25:22 +0700

    rimek komponen

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,30 +1,28 @@
-import { Input, Typography, Button } from "@material-tailwind/react";
+import { Input, Typography, Button, Card } from "@material-tailwind/react";
 
 export default function Form(): React.JSX.Element {
   return (
-    <div className="mb-4 ">
+    <Card color="transparent" className="mb-4">
       <Typography variant="lead" color="gray" className="text-white">
         Input
       </Typography>
-      <div className="mt-3 space-y-3.5">
+      <form className="mt-2 space-y-3.5">
         <div>
-          <Input label="AR Sekarang" variant="outlined" color="white" type="number" />
-          <Typography variant="small" color="red" className="mt-0.5 font-normal text-red-200">
+          <Input type="number" variant="outlined" label="AR Sekarang" color="white" />
+          <Typography color="red" className="font-normal text-red-200">
             pala bapak engkau
           </Typography>
         </div>
         <div>
-          <Input label="AR Sekarang" variant="outlined" color="white" type="number" />
-          <Typography variant="small" color="red" className="mt-0.5 font-normal text-red-200">
+          <Input type="number" variant="outlined" label="AR Sekarang" color="white" />
+          <Typography color="red" className="font-normal text-red-200">
             pala bapak engkau
           </Typography>
         </div>
-        <div>
-          <Button type="button" fullWidth onClick={() => alert("pala bapak engkau")} variant="outlined" color="teal" className="border-slate-300 py-1.5 text-lg capitalize text-slate-300 focus:ring-0">
-            Hitung
-          </Button>
-        </div>
-      </div>
-    </div>
+        <Button onClick={() => alert("pala bapak engkau")} fullWidth variant="outlined" className="border-slate-300 py-1.5 text-lg capitalize text-slate-300 focus:ring-0">
+          Hitung
+        </Button>
+      </form>
+    </Card>
   );
 }
